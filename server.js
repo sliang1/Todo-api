ca6936f8601c7e4ff668edc587efad5bb5fcb496
Shuang Liang <liangyouyou881116@gmail.com>
2016-06-22 20:19:09 -0700

    Add Post for todos

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT ||3000;
 
-var todos = [{
-	id: 1,
-	description: "Meet mom for lunch",
-	completed: false
-}, {
-	id: 2,
-	description: "Go to market",
-	completed: false
-},{
-	id: 3,
-	description: "Feed the cat",
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res)  {
@@ -22,9 +15,12 @@ app.get('/', function (req, res)  {
 
 });
 
+// GET /todos
 app.get('/todos', function(req,res) {
 	res.json(todos);
 });
+
+// GET /todos/:id
 
 app.get('/todos/:id', function (req,res) {
 	var todoId = parseInt(req.params.id,10);     //any request parameter is always a string
@@ -43,9 +39,36 @@ app.get('/todos/:id', function (req,res) {
 	// res.send("Asking for todo with id of " + req.params.id);
 });
 
+//POST /todos/:id
+
+app.post('/todos', function (req,res) {
+     var body = req.body;
+
+     body.id = todoNextId++;
+
+     todos.push(body);
+
+      console.log('description: ' + body.description);
+
+
+
+      res.json(body);
+
+});
+
+
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+var test;
+
+if(test) {
+	console.log('todo is there');
+} else {
+	console.log('false');
+}
 
 
 
