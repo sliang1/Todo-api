3c22f073a126845add85bbdfb7a9ab0327d86553
Shuang Liang <liangyouyou881116@gmail.com>
2016-06-22 21:36:27 -0700

    Add DELETE todos route

--- server.js
+++ server.js
@@ -64,6 +64,22 @@ app.post('/todos', function (req,res) {
 
 });
 
+//DELETE /todos/:id
+
+app.delete('/todos/:id', function (req,res) {
+	var todoId = parseInt(req.params.id,10);     //any request parameter is always a string
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if ( !matchedTodo) {
+		res.status(404).json({"error" : "no todo found with that id"});
+	} else {
+		todos = _.without (todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+
+	
+});
+
 
 
 app.listen(PORT, function () {
