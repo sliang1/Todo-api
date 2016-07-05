e766fb95e80b611bd8d6c28f707e67df27829174
Shuang Liang <liangyouyou881116@gmail.com>
2016-07-04 21:06:39 -0700

    Add user model and Post /users request

--- server.js
+++ server.js
@@ -183,7 +183,16 @@ app.put('/todos/:id', function (req,res) {
   		res.status(500).send();
   });
 
+});
 
+app.post('/users', function (req, res) {
+	var body = _.pick(req.body, 'email','password');
+
+	db.user.create(body).then(function (user) {
+		res.json(user.toJSON());
+	},function (e) {
+		res.status(400).json(e);
+	});
 });
 
 
