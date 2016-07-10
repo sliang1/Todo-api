3dd3fe8c8533afa51d940301ca277f9b08d43048
Shuang Liang <liangyouyou881116@gmail.com>
2016-07-09 21:41:01 -0700

    Adding DELETE/users/login

--- server.js
+++ server.js
@@ -219,16 +219,20 @@ app.post('/users', function (req, res) {
 // POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user){
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
 		
-	}, function () {
+	}).then (function (tokenInstance){
+		res.header('Auth',tokenInstance.get('token')).json(userInstance.toPublicJSON());
+
+	}).catch (function () {
 		res.status(401).send();
 	});
 
@@ -251,6 +255,16 @@ app.post('/users/login', function (req, res) {
 	// });
 
 
+});
+
+
+//DELETE /users/login
+app.delete('/users/login',middleware.requireAuthentication, function (req,res) {
+	req.token.destroy().then (function () {
+		res.status(204).send();
+	}).catch(function (){
+		res.status(500).send();
+	});
 });
 
 
