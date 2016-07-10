8520f04044b2056e583e20ebec8c37fc52f92726
Shuang Liang <liangyouyou881116@gmail.com>
2016-07-09 18:20:12 -0700

    Add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT ||3000;
@@ -20,7 +21,7 @@ app.get('/', function (req, res)  {
 
 
 // GET /todos?completed=true&q=work
-app.get('/todos', function (req,res) {
+app.get('/todos', middleware.requireAuthentication, function (req,res) {
 
 	var query = req.query;
 	var where = {};
@@ -64,7 +65,7 @@ app.get('/todos', function (req,res) {
 
 
 // GET /todos/:id
-app.get('/todos/:id', function (req,res) {
+app.get('/todos/:id', middleware.requireAuthentication, function (req,res) {
 	var todoId = parseInt(req.params.id,10);     //any request parameter is always a string
 
     db.todo.findById(todoId).then (function (todo) {
@@ -91,7 +92,7 @@ app.get('/todos/:id', function (req,res) {
 
 
 //POST /todos
-app.post('/todos', function (req,res) {
+app.post('/todos', middleware.requireAuthentication, function (req,res) {
      var body = req.body;
      
      body = _.pick(body, 'description', 'completed');
@@ -119,7 +120,7 @@ app.post('/todos', function (req,res) {
 
 
 //DELETE /todos/:id
-app.delete('/todos/:id', function (req,res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function (req,res) {
 	var todoId = parseInt(req.params.id,10);     //any request parameter is always a string
 
 	db.todo.destroy({
@@ -152,7 +153,7 @@ app.delete('/todos/:id', function (req,res) {
 
 
 //PUT /todos/:id
-app.put('/todos/:id', function (req,res) {
+app.put('/todos/:id', middleware.requireAuthentication, function (req,res) {
   var todoId = parseInt(req.params.id, 10);      
   var body = _.pick(req.body, 'description', 'completed');
   var attributes = {};
